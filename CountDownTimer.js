a06c4a2b186eac2d7bfccc8e3fdd8dd1c8a6323c
T.Yazadni <56879548+yazdani1994@users.noreply.github.com>
2022-05-30 18:06:08 +0430

    new

--- CountDownTimer.js
+++ CountDownTimer.js
@@ -2,18 +2,18 @@ var timerHours = 0;
 var timerMinutes = 1;
 var timerSecond = 60;
 
-var myTimer = setInterval(function() {
-    console.log(timerHours + " : " + timerMinutes + " : " + timerSecond);
-    if(timerSecond == 0){
-        timerSecond = 60;
-        timerMinutes--;
-    }
-    if(timerMinutes < 0){
-        timerMinutes = 59;
-        timerHours--;
-    }
-    timerSecond--;
-    if(timerHours == 0 && timerMinutes == 0 && timerSecond == 0){
-        clearInterval(myTimer);
-    }
-}, 100);
+var myTimer = setInterval(function () {
+  console.log(timerHours + " : " + timerMinutes + " : " + timerSecond);
+  if (timerSecond == 0) {
+    timerSecond = 60;
+    timerMinutes--;
+  }
+  if (timerMinutes < 0) {
+    timerMinutes = 59;
+    timerHours--;
+  }
+  timerSecond--;
+  if (timerHours == 0 && timerMinutes == 0 && timerSecond == 0) {
+    clearInterval(myTimer);
+  }
+}, 1000);
